7b9fa066831a73b26b1bfdc08a525f496782f0b3
Stephen Hurley <stephenhurley@gmail.com>
2019-04-20 10:12:53 -0700

    Cleaned up component class.

--- src/component.js
+++ src/component.js
@@ -1,11 +1,11 @@
-class Component {
+export default class Component {
 	/**
 	 * Constructs a component inside the parent element.
 	 * @param {HTMLElement} elem
 	 */
 	constructor(elem) {
 		/**
-		 * The div element.
+		 * The HTML element that this uses.
 		 * @type {HTMLElement}
 		 * @private
 		 */
@@ -16,19 +16,28 @@ class Component {
 		 * @type {HTMLStyleElement}
 		 * @private
 		 */
-		this._style = document.querySelector('head style#' + this.constructor.name);
+		this._styleElem = document.querySelector('head style#' + this.constructor.name);
+
+		// Set the html.
+		if (this.constructor.html !== undefined) {
+			this._elem.innerHTML = this.constructor.html;
+		}
+		else {
+			this._elem.innerHTML = '';
+		}
 
 		// Add the component's name to the class list.
 		this._elem.classList.add(this.constructor.name);
 
 		// Create the style if it doesn't already exist, and increment the use count.
-		if (this._style === null) {
-			this._style = document.createElement('style');
-			this._style.id = this.constructor.name;
-			this._style.attributes['useCount'] = 0;
-			document.head.appendChild(this._style);
+		if (this._styleElem === null && this.constructor.style !== undefined) {
+			this._styleElem = document.createElement('style');
+			this._styleElem.id = this.constructor.name;
+			this._styleElem.innerHTML = this.constructor.style;
+			this._styleElem.attributes['useCount'] = 0;
+			document.head.appendChild(this._styleElem);
 		}
-		this._style.attributes['useCount'] += 1;
+		this._styleElem.attributes['useCount'] += 1;
 	}
 
 	/**
@@ -36,9 +45,11 @@ class Component {
 	 */
 	destroy() {
 		// Decrement the use count of the style element and remove it if the use count is zero.
-		this._style.attributes['useCount'] -= 1;
-		if (this._style.attributes['useCount'] === 0) {
-			document.head.removeChild(this._style);
+		if (this.constructor.style !== undefined) {
+			this._styleElem.attributes['useCount'] -= 1;
+			if (this._styleElem.attributes['useCount'] === 0) {
+				document.head.removeChild(this._styleElem);
+			}
 		}
 
 		// Remove the component's name from the class list.
@@ -49,53 +60,22 @@ class Component {
 	}
 
 	/**
-	 * Returns the style that the component uses.
-	 * @returns {string}
+	 * Returns the HTML element that this uses.
+	 * @returns {HTMLElement}
 	 */
-	get __style() {
-		return this._style.innerHTML;
-	}
-
-	/**
-	 * Sets the style that the component uses.
-	 * @param {string} style
-	 */
-	set __style(style) {
-		// Set the style if this is the first element of its type.
-		if (this._style.attributes['useCount'] === 1) {
-			this._style.innerHTML = style;
-		}
-	}
-
-	/**
-	 * Returns the html that the component uses.
-	 * @returns {string}
-	 */
-	get __html() {
-		return this._elem.innerHTML;
-	}
-
-	/**
-	 * Sets the html that the component uses.
-	 * @param {string} html
-	 */
-	set __html(html) {
-		this._elem.innerHTML = html;
-	}
-
-	/**
-	 * Performs query selector on the component.
-	 * @param {string} selector
-	 * @param {boolean} [all=false]
-	 */
-	__query(selector, all = false) {
-		if (all) {
-			return this._elem.querySelectorAll(selector);
-		}
-		else {
-			return this._elem.querySelector(selector);
-		}
+	get elem() {
+		return this.elem;
 	}
 }
 
-export default Component;
+/**
+ * The HTML string, which is overridden by the subclass.
+ * @type {string}
+ */
+Component.html = '';
+
+/**
+ * The style string, which is overidden by the subclass.
+ * @type {string}
+ */
+Component.style = '';
