bf8f8da4e73be2b823aaa881dffa6515d28732f7
Stephen Hurley <stephenhurley@gmail.com>
2019-05-02 10:55:32 -0700

    Fixed UIComponent to make a style element for every ancestor.

--- src/ui_component.js
+++ src/ui_component.js
@@ -1,3 +1,6 @@
+/**
+ * A base UI component from which other components can extend.
+ */
 export default class UIComponent {
 	/**
 	 * Constructs a component inside the parent element.
@@ -5,66 +8,62 @@ export default class UIComponent {
 	 */
 	constructor(elem) {
 		/**
-		 * The HTML element that this uses.
+		 * The HTML element that this component uses.
 		 * @type {HTMLElement}
 		 * @private
 		 */
 		this._elem = elem;
 
-		/**
-		 * The style element, which is added to the head, if there is style.
-		 * @type {HTMLStyleElement}
-		 * @private
-		 */
-		this._styleElem = document.querySelector('head style#' + this.constructor.name);
-
-		// Set the html.
-		if (this.constructor.html !== undefined) {
-			this._elem.innerHTML = this.constructor.html;
-		}
-		else {
-			this._elem.innerHTML = '';
-		}
-
-		// Add the component's name and its ancestors to the class list.
+		// Go through each of the component's ancestors,
 		let thisAncestor = this;
 		while (thisAncestor.constructor !== UIComponent) {
+			// Add the ancestor's name to the class list.
 			this._elem.classList.add(thisAncestor.constructor.name);
 			thisAncestor = Object.getPrototypeOf(thisAncestor);
+
+			// Create the ancestor's style element if it doesn't already exist, and increment the use count.
+			if (thisAncestor.constructor.style !== undefined && thisAncestor.constructor.style !== '') {
+				let styleElem = document.querySelector('head style#' + thisAncestor.constructor.name);
+				if (styleElem === null) {
+					styleElem = document.createElement('style');
+					styleElem.id = thisAncestor.constructor.name;
+					styleElem.attributes['useCount'] = 0;
+					styleElem.innerHTML = thisAncestor.constructor.style;
+					document.head.appendChild(styleElem);
+				}
+				styleElem.attributes['useCount'] += 1;
+			}
 		}
 
-		// Create the style if it doesn't already exist, and increment the use count.
-		if (this._styleElem === null && this.constructor.style !== undefined) {
-			this._styleElem = document.createElement('style');
-			this._styleElem.id = this.constructor.name;
-			this._styleElem.attributes['useCount'] = 0;
-			this._styleElem.innerHTML = this.constructor.style;
-			document.head.appendChild(this._styleElem);
-		}
-		this._styleElem.attributes['useCount'] += 1;
+		// Set the html.
+		this._elem.innerHTML = this.constructor.html || '';
 	}
 
 	/**
 	 * Destroys this when it is no longer needed. Call to clean up the object.
 	 */
 	destroy() {
-		// Decrement the use count of the style element and remove it if the use count is zero.
-		if (this.constructor.style !== undefined) {
-			this._styleElem.attributes['useCount'] -= 1;
-			if (this._styleElem.attributes['useCount'] === 0) {
-				document.head.removeChild(this._styleElem);
-			}
-		}
-
-		// Remove the component's name and its ancestors from the class list.
-		let thisAncestor = this;
-		while (thisAncestor.constructor !== UIComponent) {
-			this._elem.classList.remove(thisAncestor.constructor.name);
-			thisAncestor = Object.getPrototypeOf(thisAncestor);
-		}
-
 		// Clear out any html from the parent element.
 		this._elem.innerHTML = '';
+
+		// Go through each of the component's ancestors,
+		let thisAncestor = this;
+		while (thisAncestor.constructor !== UIComponent) {
+			// Remove the ancestor's name from the class list.
+			this._elem.classList.remove(thisAncestor.constructor.name);
+			thisAncestor = Object.getPrototypeOf(thisAncestor);
+
+			// Decrement the use count of the ancestor's style element and remove it if the use count is zero.
+			if (thisAncestor.constructor.style !== undefined) {
+				let styleElem = document.querySelector('head style#' + thisAncestor.constructor.name);
+				if (styleElem) {
+					styleElem.attributes['useCount'] -= 1;
+					if (styleElem.attributes['useCount'] === 0) {
+						document.head.removeChild(styleElem);
+					}
+				}
+			}
+		}
 	}
 
 	/**
